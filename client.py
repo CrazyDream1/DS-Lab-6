b2752c562f59e71fea9562b8dfa69c792db34252
CrazyDream1 <35272226+CrazyDream1@users.noreply.github.com>
2020-09-21 22:37:50 +0300

    all

--- client.py
+++ client.py
@@ -2,23 +2,34 @@ import socket
 import sys
 from pathlib import Path
 
+# Get parameters from console
 filename = sys.argv[1]
 host = sys.argv[2]
 port = int(sys.argv[3])
 
+# Get size of file in bytes
 filesize = Path(filename).stat().st_size
 currentfilesize = 0
+
+# Create socket
 s = socket.socket(socket.AF_INET)
+# Connect to server
 s.connect((host,port))
+
+# Send name of file
 s.send(bytes(filename, 'utf-8'))
+# Send file data
 f = open (filename, "rb")
 l = f.read(1024)
+
 while (l):
     currentfilesize = currentfilesize + 1024
+    # Each 100Kb write progress
     if (currentfilesize % (1024 * 100) == 0):
         print(str(currentfilesize / filesize * 100) + "%")
     s.send(l)
     l = f.read(1024)
+    
 s.close()
 
-# python client.py car.png localhost 8888
+# python client.py car.png "18.188.141.97" 8888
--- server/server.py
+++ server/server.py
@@ -1,19 +1,31 @@
 import socket
 import sys
+import os
 
-host = 'localhost'
+# Create socket
+host = ''
 port = 8888
 addr = (host, port)
 server = socket.socket(socket.AF_INET)
 server.bind(addr)
 server.listen(10)
 
+# For each connection
 while True:
-    sc, address = server.accept()
-	
-    filename = sc.recv(1024).decode("utf-8")
-    f = open(filename,'wb') #open in binary  
+    sc, address = server.accept() # Connect to client
+    filename = sc.recv(1024).decode("utf-8") # Get name of file
     
+    ls = os.listdir('.') # get list of files in the directory
+    if filename in ls: # if file is already exists set name as "namecopy_1.extension"
+        dot_split = filename.split('.')
+        name = dot_split[0]
+        extension = ".".join(dot_split[1:])
+        i = 1
+        while(filename in ls):
+            filename = name + "copy_" + str(i) + "." + extension # change filename
+            i += 1    
+	
+    f = open(filename,'wb') #open in binary
     # receive data and write it to file
     l = sc.recv(1024)
     while (l):
@@ -23,4 +35,4 @@ while True:
     f.close()
     sc.close()
 
-s.close()
+server.close()
